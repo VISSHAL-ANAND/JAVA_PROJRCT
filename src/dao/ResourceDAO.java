f1fdd73fafa96df10a4f443fa0aeaa347ec4179f
AV <visshalsingiri@gmail.com>
2026-09-23 15:19:11 +0530

    Complete resource DAO

--- src/dao/ResourceDAO.java
+++ src/dao/ResourceDAO.java
@@ -1,12 +1,12 @@
 package dao;
 
 import database.DBConnection;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResourceDAO {
-
     public List<Integer> findAvailableResourceIds() throws SQLException {
         String sql = "SELECT id FROM resources WHERE available = TRUE AND quantity > 0 ORDER BY id";
         List<Integer> ids = new ArrayList<>();
@@ -14,10 +14,7 @@ public class ResourceDAO {
         try (Connection connection = DBConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet rs = statement.executeQuery()) {
-
-            while (rs.next()) {
-                ids.add(rs.getInt("id"));
-            }
+            while (rs.next()) ids.add(rs.getInt("id"));
         }
         return ids;
     }
