0c7f4e855bc21893edb2aad1aaa06236d1378b4f
AV <visshalsingiri@gmail.com>
2026-09-23 15:27:51 +0530

    Add maintenance staff model

--- src/model/MaintenanceStaff.java
+++ src/model/MaintenanceStaff.java
@@ -1,22 +1,13 @@
 package model;
 
 public class MaintenanceStaff extends User {
-    private String specialization;
-    private boolean available;
-
-    public MaintenanceStaff(int id, String name, String email, String password,
-                            String specialization, boolean available) {
-        super(id, name, email, password);
-        this.specialization = specialization;
-        this.available = available;
+    private String specialization; private boolean available;
+    public MaintenanceStaff(int id, String name, String email, String password, String specialization, boolean available) {
+        super(id, name, email, password); this.specialization = specialization; this.available = available;
     }
-
     public String getSpecialization() { return specialization; }
     public boolean isAvailable() { return available; }
-
     public void setSpecialization(String specialization) { this.specialization = specialization; }
     public void setAvailable(boolean available) { this.available = available; }
-
-    @Override
-    public String getRole() { return Role.MAINTENANCE.name(); }
+    @Override public String getRole() { return Role.MAINTENANCE.name(); }
 }
