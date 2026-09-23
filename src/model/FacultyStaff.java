37e1e543c99f649704bb73a075a87a173ca68ae7
AV <visshalsingiri@gmail.com>
2026-09-23 15:27:46 +0530

    Add faculty staff model

--- src/model/FacultyStaff.java
+++ src/model/FacultyStaff.java
@@ -2,15 +2,10 @@ package model;
 
 public class FacultyStaff extends User {
     private String department;
-
     public FacultyStaff(int id, String name, String email, String password, String department) {
-        super(id, name, email, password);
-        this.department = department;
+        super(id, name, email, password); this.department = department;
     }
-
     public String getDepartment() { return department; }
     public void setDepartment(String department) { this.department = department; }
-
-    @Override
-    public String getRole() { return Role.FACULTY.name(); }
+    @Override public String getRole() { return Role.FACULTY.name(); }
 }
