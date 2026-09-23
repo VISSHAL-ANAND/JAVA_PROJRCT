dc2765b2ee3bf2899123ad035a75f7b0ffc81bf7
AV <visshalsingiri@gmail.com>
2026-09-23 15:11:19 +0530

    Add Admin model

---
+++ src/model/Admin.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Admin extends User {
+    public Admin(int id, String name, String email, String password) {
+        super(id, name, email, password);
+    }
+
+    @Override
+    public String getRole() { return "ADMIN"; }
+}
