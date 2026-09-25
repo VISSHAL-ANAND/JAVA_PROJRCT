279603fffaa9acd54bb9f54e89fc22dbe9f8e1f6
AV <visshalsingiri@gmail.com>
2026-09-25 11:44:09 +0530

    Start HTTP API alongside socket server

--- src/network/ServerMain.java
+++ src/network/ServerMain.java
@@ -1,14 +1,19 @@
 package network;
 
+import api.ApiServer;
+
 public class ServerMain {
     public static void main(String[] args) {
-        int port = args.length > 0 ? Integer.parseInt(args[0]) : 5050;
+        int socketPort = args.length > 0 ? Integer.parseInt(args[0]) : 5050;
         int poolSize = args.length > 1 ? Integer.parseInt(args[1]) : 10;
+        int apiPort = args.length > 2 ? Integer.parseInt(args[2]) : 8080;
 
         try {
-            new CampusServer(port, poolSize).start();
+            ApiServer apiServer = new ApiServer(apiPort);
+            apiServer.start();
+            new CampusServer(socketPort, poolSize).start();
         } catch (Exception e) {
-            System.err.println("Unable to start CAMPUSOS server: " + e.getMessage());
+            System.err.println("Unable to start CAMPUSOS servers: " + e.getMessage());
         }
     }
 }
